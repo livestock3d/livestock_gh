a1b3fe3dbf06fff88e9344137d9cf8dadeac162c
Christian Kongsgaard <ocni@dtu.dk>
2017-05-25 14:35:43 +0200

    Add files via upload

--- python/templates/DrainPoolsTemplate.py
+++ python/templates/DrainPoolsTemplate.py
@@ -2,7 +2,7 @@ user = open('/mnt/c/livestock/python/templates/user.txt','r').readline()
 
 # Import
 import sys
-sys.path.insert(0, "/home/" + user + "/classes")
+sys.path.insert(0, "/home/" + user + "/livestock/classes")
 import RainClasses as rc
 
 print('reached template')
