8a35b557968c179f1afafff9ab7db448e117b408
ocni-dtu <ocni@dtu.dk>
2017-05-29 14:07:35 +0200

    move from livestock3d to ocni-dtu

--- python/templates/SSHTemplate.py
+++ python/templates/SSHTemplate.py
@@ -28,7 +28,6 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect(ip,port=port,username=user,password=pw)
 print('Opening connection')
-print('remote',remotefolder[-10:] + '$ ')
 
 # Copy files to remove server
 sftp = ssh.open_sftp()
@@ -39,35 +38,63 @@ channel = ssh.invoke_shell()
 
 channel_data = ''
 
+com_send = False
+folder_send = False
+return_send = False
+outfile = False
+
 while True:
+    # Print shell
     if channel.recv_ready():
-        channel_old = channel_data
         channel_bytes = channel.recv(9999)
         channel_data += channel_bytes.decode("utf-8")
-        if not channel_old == channel_data:
-            print(channel_data)
+        print(channel_data)
 
     else:
         pass
-    if channel_data.endswith('ocni@KONGSGAARD-PC:~$ '):
+
+    # Execute commands
+    if not folder_send:
+        sftp.chdir(remotefolder)
         channel.send('cd ' + remotefolder + '\n')
         print('folder send')
+        folder_send = True
 
-    elif channel_data.endswith(remotefolder[-10:] + '$ '):
+    elif folder_send and not com_send:
         channel.send('python ' + run + '\n')
         print('command send')
+        com_send = True
 
-    elif os.path.isfile(localfolder + '\\out.txt'):
-        print('Succes!')
+    else:
+        pass
+
+    # Look for outfile
+    try:
+        outfile = sftp.file(remotefolder + '/out.txt')
+    except:
+        pass
+
+    if outfile:
+        print('Found out file')
+        sftp.get(remotefolder + '/out.txt',localfolder + '\\out.txt')
+        sftp.remove('out.txt')
+
+    # If found start transfering files and clean up
+    if os.path.isfile(localfolder + '\\out.txt'):
+        #print('Succes!')
 
         # Copy result files to local and delete remotely
+        print('Copying and deleting result files')
         for f in ret:
-            sftp.get(remotefolder + '/' + f)
-            channel.send('rm ' + remotefolder + '/' + f)
+            sftp.get(remotefolder + '/' + f, localfolder + '/' + f)
+            #print(f)
+            sftp.remove(f)
 
         # Delete input files
+        print('Deleting input files')
         for f in trans:
-            channel.send('rm ' + remotefolder + '/' + f)
+            #print(f)
+            sftp.remove(f)
 
         break
 
