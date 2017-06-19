457d1c94d90b7cec2beb5d1bd338a56d1c26d4fd
ocni-dtu <ocni@dtu.dk>
2017-06-19 11:10:11 +0200

    Update RainClasses

--- python/classes/RainClasses.py
+++ python/classes/RainClasses.py
@@ -305,6 +305,7 @@ def drainPools(path):
             bFace = array(bFace)
             bVox = array(bVox)
             bMesh = pm.form_mesh(bVert,bFace,bVox)
+            pm.save_mesh(meshName, bMesh)
 
             # Make intersection
             newMesh = pm.boolean(mesh,bMesh,'intersection',engine='igl')
