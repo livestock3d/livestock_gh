5549e68e9c46a0719f312bd9e07f1c555fb6fc86
ocni-dtu <ocni@dtu.dk>
2017-07-12 16:08:17 +0200

    Update to target resolution

--- python/classes/GeometryClasses.py
+++ python/classes/GeometryClasses.py
@@ -9,7 +9,7 @@ def fix_mesh(mesh, detail="normal"):
     elif detail == "high":
         target_len = diag_len * 5e-3
     elif detail == "low":
-        target_len = diag_len * 0.04
+        target_len = diag_len * 0.03
     print("Target resolution: {} mm".format(target_len))
 
     count = 0
