e60befb9124137e71336edcba33a21b887d5d935
ocni-dtu <ocni@dtu.dk>
2017-08-16 11:49:07 +0200

    Edit drainpaths

--- python/classes/RainClasses.py
+++ python/classes/RainClasses.py
@@ -102,6 +102,7 @@ def drainMeshPaths(meshPath,cpus):
                     v0, v1, v2 = faceVertices(index)
                     pt = gc.lowestFaceVertex(v0, v1, v2)
                     if len(adjacents) < 3:
+                        particles.append(pt)
                         index = overEdge(pt)
                         pt = startPoints[index][1]
                     else:
