08a27dbb3a172da7856a2ce81349245a94ddfd05
ocni-dtu <ocni@dtu.dk>
2017-09-14 13:40:59 +0200

    Update to SSHClasses

--- python/classes/SSHClasses.py
+++ python/classes/SSHClasses.py
@@ -8,39 +8,121 @@ __status__ = "Work in Progress"
 #----------------------------------------------------------------------------------------------------------------------#
 #Functions and Classes
 
-from paramiko import client
-import time
+# Import
 import os
+import paramiko
 
-class ssh:
+def sshConnection():
 
-    client = None
+    # Open input text file
+    localfolder = r'C:\livestock\python\ssh'
+    inData = '\\InData.txt'
 
-    def __init__(self, address, port, username, password):
-        # Let the user know we're connecting to the server
-        print("Connecting to server.")
-        # Create a new SSH client
-        self.client = client.SSHClient()
-        # The following line is required if you want the script to be able to access a server that's not yet in the known_hosts file
-        self.client.set_missing_host_key_policy(client.AutoAddPolicy())
-        # Make the connection
-        self.client.connect(address,port=port, username=username, password=password, look_for_keys=False)
+    file_obj = open(localfolder + inData, 'r')
+    data = file_obj.readlines()
+    file_obj.close()
 
-    def sendCommand(self, command):
-        if (self.client):
-            stdin, stdout, stderr = self.client.exec_command(command)
-            while not stdout.channel.exit_status_ready():
-                # Print data when available
-                if stdout.channel.recv_ready():
-                    alldata = stdout.channel.recv(1024)
-                    prevdata = b"1"
-                    while prevdata:
-                        prevdata = stdout.channel.recv(1024)
-                        alldata += prevdata
+    # Get data
+    ip = data[0][:-1]
+    port = int(data[1][:-1])
+    user = data[2][:-1]
+    pw = data[3][:-1]
+    trans = data[4][:-1].split(',')
+    run = data[5][:-1]
 
-                    print(str(alldata, "utf8"))
-                    errdata = stderr.channel.recv(1024)
-                    print(stderr)
-                    print(str(errdata, "utf8"))
+    remotefolder = '/home/' + user + '/livestock/templates'
+
+
+    # Start SSH session
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh.connect(ip,port=port,username=user,password=pw)
+    print('Opening connection')
+
+    # Copy files to remove server
+    sftp = ssh.open_sftp()
+    for f in trans:
+        sftp.put(localfolder + '/' + f, remotefolder + '/' + f)
+    sftp.put(localfolder + '/InData.txt', remotefolder + '/InData.txt')
+
+    channel = ssh.invoke_shell()
+
+    channel_data = ''
+
+    com_send = False
+    folder_send = False
+    return_send = False
+    outfile = False
+
+    while True:
+        # Print shell
+        if channel.recv_ready():
+            channel_bytes = channel.recv(9999)
+            channel_data += channel_bytes.decode("utf-8")
+            print(channel_data)
+
         else:
-            print("Connection not opened.")
+            pass
+
+        # Execute commands
+        if not folder_send:
+            sftp.chdir(remotefolder)
+            channel.send('cd ' + remotefolder + '\n')
+            print('folder send')
+            folder_send = True
+
+        elif folder_send and not com_send:
+            channel.send('python ' + run + '\n')
+            print('command send')
+            com_send = True
+
+        else:
+            pass
+
+        # Look for outfile
+        try:
+            outfile = sftp.file(remotefolder + '/out.txt')
+        except:
+            pass
+
+        if outfile:
+            print('Found out file')
+            sftp.get(remotefolder + '/out.txt', localfolder + '\\out.txt')
+            sftp.remove('out.txt')
+
+        # If found start transfering files and clean up
+        if os.path.isfile(localfolder + '\\out.txt'):
+            # print('Succes!')
+
+            # Copy result files to local and delete remotely
+            print('Copying and deleting result files')
+
+            # Get return files
+            sftp.get(remotefolder + '/InData.txt', localfolder + '/InData.txt')
+            file_obj = open(localfolder + inData, 'r')
+            data = file_obj.readlines()
+            file_obj.close()
+            ret = data[6].split(',')
+            sftp.remove('InData.txt')
+
+            for f in ret:
+                print(f)
+                sftp.get(remotefolder + '/' + f, localfolder + '/' + f)
+                sftp.remove(f)
+
+            # Delete input files
+            print('Deleting input files')
+            for f in trans:
+                # print(f)
+                sftp.remove(f)
+
+            break
+
+        else:
+            pass
+
+    # Close connection
+    print('Closing connection!')
+    sftp.close()
+    ssh.close()
