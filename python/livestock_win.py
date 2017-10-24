dff987d8fcd7996b78b809c0206ef5153429ce88
ocni-dtu <ocni@dtu.dk>
2017-10-24 18:43:33 +0200

    update to folder structure

---
+++ python/livestock_win.py
@@ -0,0 +1,15 @@
+"""
+Module to collect all Windows specific modules for Livestock.
+"""
+
+__author__ = "Christian Kongsgaard"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Christian Kongsgaard"
+__email__ = "ocni@dtu.dk"
+__status__ = "Work in Progress"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Imports
+from win import *
