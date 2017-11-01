f357869d1acf337f43cc59ee703ab562fa52c55f
ocni-dtu <ocni@dtu.dk>
2017-11-01 13:46:08 +0100

    added livestock_gh

---
+++ python/livestock_gh.py
@@ -0,0 +1,17 @@
+"""
+Module to collect all Grasshopper functions for Livestock.
+"""
+
+__author__ = "Christian Kongsgaard"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Christian Kongsgaard"
+__email__ = "ocni@dtu.dk"
+__status__ = "Work in Progress"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Imports
+from gh.geometry import *
+from gh.misc import *
+from gh.ssh import *
