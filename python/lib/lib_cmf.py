3ce381f3cf6fc9564f6b52cb5d5fa6d05b065b7f
ocni-dtu <ocni@dtu.dk>
2017-11-10 08:56:40 +0100

    bug fix Shuttleworh-Wallace

--- python/lib/lib_cmf.py
+++ python/lib/lib_cmf.py
@@ -289,7 +289,7 @@ class CMFModel:
             #cell.install_connection(cmf.PenmanMonteithET)
 
             # Install Shuttleworth-Wallace method to calculate evapotranspiration
-            cmf.ShuttleworthWallace().use_for_cell(cell)
+            cmf.ShuttleworthWallace(cell).use_for_cell(cell)
 
         return True
 
