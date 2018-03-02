f740475acab1a063c0ae6b902908de8cc1de060f
ocni-dtu <ocni@dtu.dk>
2018-03-02 20:59:39 +0100

    update the sphinx docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -17,9 +17,23 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+from unittest.mock import MagicMock
+#sys.path.insert(0, os.path.abspath('../python'))
+sys.path.insert(0, r'C:\Users\Christian\AppData\Roaming\McNeel\Rhinoceros\5.0\scripts')
+#sys.path.insert(0, r'C:\Program Files (x86)\IronPython 2.7')
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+
+MOCK_MODULES = ['System', 'Grasshopper', 'Rhino', 'scriptcontext', 'Grasshopper.Kernel', 'Grasshopper.Kernel.Data',
+                'Rhino.Geometry', 'rhinoscriptsyntax', 'System.Threading', 'System.Threading.Tasks.Parallel',
+                'Rhino.Geometry.Brep']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration ------------------------------------------------
