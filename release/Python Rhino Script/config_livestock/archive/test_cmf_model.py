0a64fbccb566fafd6156c3123329c06d1e3d5bf8
ocni-dtu <ocni@dtu.dk>
2018-09-15 10:26:56 +0200

    updated for release

--- release/Python Rhino Script/config_livestock/archive/test_cmf_model.py
+++
@@ -1,8 +0,0 @@
-from lib.cmf import CMFModel
-
-folder = r'C:\Users\Christian\Desktop\test_cmf\test_01'
-mesh_path = folder + '/mesh.obj'
-weather_path = folder + '/weather.xml'
-m = CMFModel(mesh_path, weather_path)
-
-m.create_weather()
